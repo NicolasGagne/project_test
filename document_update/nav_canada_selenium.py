b72e64e4297cfa5b4c5ed0f895c92213bf8d2984
NicolasGagne <ngagne966@hotmail.com>
2018-09-21 10:13:44 -0400

    cleanup and modifid to work with const file

--- document_update/nav_canada_selenium.py
+++ document_update/nav_canada_selenium.py
@@ -1,25 +1,27 @@
-import time
+
 from selenium import webdriver
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-def download_cfs():
+from document_update.utility import wait_download_finish
+from document_update.const import NAV_CANADA_DOWNLOAD_PAGE, NAV_CANADA_LOGIN_URL, NAV_CANADA_CREDIENTIAL
+
+def download_cfs(temp_dir):
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option("prefs", {"download.default_directory": r"C:\Users\ngagne\Documents\Python\project_test\document_update",
+    print(temp_dir)
+    chrome_options.add_experimental_option("prefs", {"download.default_directory": temp_dir,
                                               "download.prompt_for_download": False,
                                               "download.directory_upgrade": True,
                                               "safebrowsing.enabled": True
                                               })
     driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.minimize_window()
 
-    NAV_CANADA_LOGIN = "https://checkout.na3.netsuite.com/c.1063417/checkout-2-05-0/index.ssp?is=login&origin=defaultbehavior&whence=#login-register"
-    NAV_CANADA_DOWNLOAD_PAGE = "https://checkout.na3.netsuite.com/app/site/hosting/scriptlet.nl?script=12&deploy=1&compid=1063417&custpage_sel_lang=en_CA&whence="
-    payload = {'email': "yquellais@hotmail.com", 'password': "star9025", 'redirect': "true", }
-    driver.get(NAV_CANADA_LOGIN)
-    driver.find_element_by_id("login-email").send_keys("yquellais@hotmail.com")
-    driver.find_element_by_id('login-password').send_keys("star9025")
+    driver.get(NAV_CANADA_LOGIN_URL)
+    driver.find_element_by_id("login-email").send_keys(NAV_CANADA_CREDIENTIAL["email"])
+    driver.find_element_by_id('login-password').send_keys(NAV_CANADA_CREDIENTIAL["password"])
     driver.find_element_by_id('login-view').find_element_by_tag_name('button').click()
 
     WebDriverWait(driver, 10).until(EC.title_contains("Home - NetSuite Canada (NAV CANADA)"))
@@ -27,18 +29,16 @@ def download_cfs():
     driver.get(NAV_CANADA_DOWNLOAD_PAGE)
     cfs_link = driver.find_element_by_id('custpage_ns_rela_uoo_list_splits').find_elements_by_tag_name('tr')
 
-
-    print("cfs_link", len(cfs_link))
     x = 0
     for link in cfs_link:
-        x = x + 1
-        print('link', link)
-
         try:
             link.find_element_by_tag_name('a').click()
-
-
+            x = x + 1
         except:
-            print('no working ', x)
+            print('Link Not Working ', link)
+    print(x, " CFS Downloading...")
 
-    time.sleep(300)
+    wait_download_finish(temp_dir)
+
+    driver.quit()
+    print("CFS downloads completed.")
