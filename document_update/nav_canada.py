2be98e27cba3ff475921127c43a76fab1831148e
NicolasGagne <ngagne966@hotmail.com>
2018-09-21 10:13:31 -0400

    cleanup and modifid to work with const file

--- document_update/nav_canada.py
+++ document_update/nav_canada.py
@@ -2,36 +2,27 @@
 
 import requests
 
-
-AIP_AD_ENG = "http://www.navcanada.ca/EN/products-and-services/Documents/AIP/Current/part_3_ad/3ad_eng.pdf"
-
-
-
-AIP_URL = {
-    'AIP_GEN_ENG': "http://www.navcanada.ca/EN/products-and-services/Documents/AIP/Current/part_1_gen/1gen_eng.pdf",
-    'AIP_ENR_ENG':"http://www.navcanada.ca/EN/products-and-services/Documents/AIP/Current/part_2_enr/2enr_eng.pdf",
-    'AIP_AD_ENG': "http://www.navcanada.ca/EN/products-and-services/Documents/AIP/Current/part_3_ad/3ad_eng.pdf",
-    'AIP_SUP_ENG':"http://www.navcanada.ca/EN/products-and-services/Documents/AIP/Current/part_4_aip_sup/4aip_sup_eng.pdf",
-    'AIP_AIC_ENG':"http://www.navcanada.ca/EN/products-and-services/Documents/AIP/Current/part_5_aic/5aic_eng.pdf"
-    }
-
-def download_aip():
-
-    for key, value in AIP_URL.items():
+def download_aip(aip_url, temp_dir):
+    print('AIP downloading...')
+    for key, value in aip_url.items():
         responce = requests.get(value)
-        with open(key + '.pdf', 'wb') as file:
+        with open(temp_dir + "/" + key + '.pdf', 'wb') as file:
             file.write(responce.content)
+    print('AIP download completed.')
 
-def download_aim():
-    responce = requests.get("http://www.tc.gc.ca/media/documents/ca-publications/AIM-2018-1-E-ACCESS.pdf")
-    with open("Aeronautical Information Manual (AIM)" + '.pdf', 'wb') as file:
+def download_aim(aim_url, temp_dir):
+    print('AIM downloading...')
+    responce = requests.get(aim_url)
+    with open(temp_dir + "/Aeronautical Information Manual (AIM)" + '.pdf', 'wb') as file:
+        file.write(responce.content)
+    print('AIM download completed.')
+
+
+
+def download_aa(aa_url, temp_dir):
+    print('Aeronautic Act downloading...')
+    responce = requests.get(aa_url)
+    with open(temp_dir + "/Aeronautical Act" + '.pdf', 'wb') as file:
         file.write(responce.content)
 
-
-
-def download_aa():
-    responce = requests.get("http://laws-lois.justice.gc.ca/PDF/A-2.pdf")
-    with open("Aeronautical Act" + '.pdf', 'wb') as file:
-        file.write(responce.content)
-
-download_aa()
+    print('Aeronautic Act download completed.')
