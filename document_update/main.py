ac5adb66c31c11dd471ab1863ebaed1f15110778
NicolasGagne <ngagne966@hotmail.com>
2018-09-21 18:24:18 -0400

    Put mkdir in a loop.

--- document_update/main.py
+++ document_update/main.py
@@ -22,19 +22,18 @@ if __name__ == "__main__":
         "aa_dir": temp_dir + "/aa",
         "aim_dir": temp_dir + "/aim"
     }
-    try:
-        os.mkdir(temp_dir_dict['temp_dir'])
-        os.mkdir(temp_dir_dict['cfs_dir'])
-        os.mkdir(temp_dir_dict['aip_dir'])
-        os.mkdir(temp_dir_dict['aa_dir'])
-        os.mkdir(temp_dir_dict['aim_dir'])
-    except FileExistsError:
-        pass
+
+    for key, value in temp_dir_dict.items():
+        try:
+            print("create folder: ", value)
+            os.mkdir(value)
+        except FileExistsError:
+            print("Folder all ready exit")
     #download_cfs(temp_dir_dict['cfs_dir'])
     #download_aip(AIP_URL, temp_dir_dict['aip_dir'])
     #download_aim(AIM_URL, temp_dir_dict['aim_dir'])
     #download_aa(AA_URL, temp_dir_dict['aa_dir'])
-    Arinc(temp_dir_dict)
+    #Arinc(temp_dir_dict)
 
     print("DONE")
     time.sleep(300)
