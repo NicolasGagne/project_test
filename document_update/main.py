fc1f5f3b7f6d40fcc2f6001668c6469e3a6082e4
NicolasGagne <ngagne966@hotmail.com>
2018-09-21 12:42:55 -0400

    Create Dict for location of file

--- document_update/main.py
+++ document_update/main.py
@@ -3,7 +3,7 @@ from document_update.nav_canada_selenium import download_cfs
 from document_update.nav_canada import download_aip, download_aa, download_aim
 from document_update.const import *
 from document_update.arinc import Arinc
-import os, shutil
+import os, time
 
 base_dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,16 +12,32 @@ base_dir_path = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
 
-    temp_dir = base_dir_path + "/temp"
+
+    temp_dir = (base_dir_path + "/temp")
+
+    temp_dir_dict = {
+        "temp_dir": temp_dir,
+        "cfs_dir": temp_dir + "/cfs",
+        "aip_dir": temp_dir + "/api",
+        "aa_dir": temp_dir + "/aa",
+        "aim_dir": temp_dir + "/aim"
+    }
     try:
-        os.mkdir(temp_dir)
+        os.mkdir(temp_dir_dict['temp_dir'])
+        os.mkdir(temp_dir_dict['cfs_dir'])
+        os.mkdir(temp_dir_dict['aip_dir'])
+        os.mkdir(temp_dir_dict['aa_dir'])
+        os.mkdir(temp_dir_dict['aim_dir'])
     except FileExistsError:
         pass
-    #download_cfs(temp_dir)
-    #download_aip(AIP_URL, temp_dir)
-    #download_aim(AIM_URL, temp_dir)
-    #download_aa(AA_URL, temp_dir)
-    Arinc(temp_dir)
+    #download_cfs(temp_dir_dict['cfs_dir'])
+    #download_aip(AIP_URL, temp_dir_dict['aip_dir'])
+    #download_aim(AIM_URL, temp_dir_dict['aim_dir'])
+    #download_aa(AA_URL, temp_dir_dict['aa_dir'])
+    Arinc(temp_dir_dict)
+
+    print("DONE")
+    time.sleep(300)
 
 
     # remove the temp file
