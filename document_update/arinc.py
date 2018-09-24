18c98d289b3d3cab7890ae82d76a5ed80d8bdc81
NicolasGagne <ngagne966@hotmail.com>
2018-09-24 16:16:55 -0400

    close driver at the end of arinc

--- document_update/arinc.py
+++ document_update/arinc.py
@@ -1,21 +1,4 @@
-'''import requests
 
-from document_update.const import *
-
-arinc_session = requests.Session()
-
-arinc_session.get(ARINC_LOGIN)
-
-arinc_session.post(ARINC_LOGIN, data=ARINC_CREDIENTIAL)
-
-url = "https://direct.arinc.net/ADC/ADCContext/Pdf?managedDoc=2145737"
-arinc_session.get("https://direct.arinc.net/ADC/ADCContext/NewCreateFPL")
-
-pdf = arinc_session.get(url)
-
-with open("C:/Users/ngagne/Documents/Python/project_test/document_update/" + "TEST" + ".pdf", 'wb') as doc:
-    doc.write(pdf.content)
-'''
 
 from document_update.const import ARINC_CREDIENTIAL, ARINC_LOGIN_URL, ARINC_DOC_URL
 from document_update.utility import time_sleep_counter
@@ -42,6 +25,7 @@ class Arinc():
         self.replace_cfss_in_cfs_folder()
         self.replace_aa_in_ops_folder()
         self.replace_aim_in_ops_folder()
+        self.driver.quit()
 
     def create_driver(self):
         chrome_options = webdriver.ChromeOptions()
