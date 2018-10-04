aff43b2a8134096c9be41bc16109ed4c424df902
NicolasGagne <ngagne966@hotmail.com>
2018-10-04 16:34:37 -0400

    Remove Starlink Stuff, move to there own project

--- tsa_waiver/main.py
+++
@@ -1,9 +0,0 @@
-
-
-from tsa_waiver. tsa_website import login_tsa_website, create_new_waiver, create_list_input
-
-if __name__ == "__main__":
-
-    session = login_tsa_website()
-    create_list_input(session)
-    #create_new_waiver(session)
